ff16609a65696f53a7ad6ac5fa6c0d9094d8a8b2
Markku Leiniö <markkuleinio@users.noreply.github.com>
2021-02-14 10:06:03 +0200

    Move MIB loading in load_mibs()

--- mib-browser.py
+++ mib-browser.py
@@ -63,118 +63,123 @@ def print_list(node):
 
 
 all_mibs: List[RawMib] = []
-
-mib = None
-name_waiting = None
-prev_line = None
-more_needed = False
-imported_items = []
-parsing_imports = False
-missing_items = set()
-
 imports = {}
 
-for line in sys.stdin:
-    line = line.strip()
-    cols = line.split()
-    if name_waiting:
-        if "::=" not in line:
-            continue
-        name = name_waiting
-        name_waiting = None
-        match = re.search(r"::=\s*\{\s*([\w-]+)\s+([0-9]+)\s*\}", line)
-        parent = match[1]
-        number = match[2]
-        # Added to the tree later below
-    elif parsing_imports:
-        words = re.split(r"[, ]+", line)
-        i = 0
-        while i < len(words):
-            if words[i] != "FROM":
-                if words[i]:
-                    imported_items.append(words[i])
-            else:
-                imported_from = words[i+1]
+def load_mibs():
+    mib = None
+    name_waiting = None
+    prev_line = None
+    more_needed = False
+    imported_items = []
+    parsing_imports = False
+    global all_mibs
+    global imports
+
+    for line in sys.stdin:
+        line = line.strip()
+        cols = line.split()
+        if name_waiting:
+            if "::=" not in line:
+                continue
+            name = name_waiting
+            name_waiting = None
+            match = re.search(r"::=\s*\{\s*([\w-]+)\s+([0-9]+)\s*\}", line)
+            parent = match[1]
+            number = match[2]
+            # Added to the tree later below
+        elif parsing_imports:
+            words = re.split(r"[, ]+", line)
+            i = 0
+            while i < len(words):
+                if words[i] != "FROM":
+                    if words[i]:
+                        imported_items.append(words[i])
+                else:
+                    imported_from = words[i+1]
+                    i += 1
+                    if imported_from.endswith(";"):
+                        parsing_imports = False
+                        imported_from = imported_from[:-1]
+                    for item in imported_items:
+                        imports[item] = imported_from
+                    imported_items = []
                 i += 1
-                if imported_from.endswith(";"):
-                    parsing_imports = False
-                    imported_from = imported_from[:-1]
-                for item in imported_items:
-                    imports[item] = imported_from
-                imported_items = []
-            i += 1
-        continue
-    elif line == "" or line.startswith("--") or line.find(",") >= 0 or cols[0] == "SYNTAX":
-        continue
-    elif (
-        len(cols) == 2 and cols[1] in [
-            "OBJECT-IDENTITY",
-            "OBJECT-TYPE",
-            "MODULE-IDENTITY",
-            "NOTIFICATION-TYPE",
-        ]) or (
-        len(cols) == 3 and cols[1] == "OBJECT" and cols[2] == "IDENTIFIER"
-    ):
-        # Save the name and keep looping
-        name_waiting = cols[0]
-        continue
-    elif "DEFINITIONS" in cols and "::=" in cols and "BEGIN" in cols:
-        # "mibname DEFINITIONS ::= BEGIN"
-        mib_name = cols[0]
-        if mib:
-            all_mibs.append(mib)
-        mib = RawMib(mib_name)
-        continue
-    elif more_needed:
-        line = prev_line + " " + line
-        more_needed = False
-    elif line.find("OBJECT IDENTIFIER") >= 0:
-        if line == "OBJECT IDENTIFIER":
             continue
-        elif line.find(")") >= 0:
+        elif line == "" or line.startswith("--") or line.find(",") >= 0 or cols[0] == "SYNTAX":
             continue
-        elif line.find("::=") == -1:
-            # We need to read more to find the assignment
-            more_needed = True
+        elif (
+            len(cols) == 2 and cols[1] in [
+                "OBJECT-IDENTITY",
+                "OBJECT-TYPE",
+                "MODULE-IDENTITY",
+                "NOTIFICATION-TYPE",
+            ]) or (
+            len(cols) == 3 and cols[1] == "OBJECT" and cols[2] == "IDENTIFIER"
+        ):
+            # Save the name and keep looping
+            name_waiting = cols[0]
+            continue
+        elif "DEFINITIONS" in cols and "::=" in cols and "BEGIN" in cols:
+            # "mibname DEFINITIONS ::= BEGIN"
+            mib_name = cols[0]
+            if mib:
+                all_mibs.append(mib)
+            mib = RawMib(mib_name)
+            continue
+        elif more_needed:
+            line = prev_line + " " + line
+            more_needed = False
+        elif line.find("OBJECT IDENTIFIER") >= 0:
+            if line == "OBJECT IDENTIFIER":
+                continue
+            elif line.find(")") >= 0:
+                continue
+            elif line.find("::=") == -1:
+                # We need to read more to find the assignment
+                more_needed = True
+                prev_line = line
+                continue
+            elif line.startswith("OBJECT IDENTIFIER"):
+                # Let's take the previous line as well
+                line = prev_line + " " + line
+            match = re.search(r"([\w-]+)\s*OBJECT IDENTIFIER\s*::=\s*\{\s*([\w-]+)\s*([0-9]+)\s*\}", line)
+            name = match[1]
+            parent = match[2]
+            number = match[3]
+        elif line.startswith("IMPORTS"):
+            imported_items = []
+            parsing_imports = True
+            words = re.split(r"[, ]+", line)
+            if len(words) == 1:
+                continue
+            i = 1   # Skip the first word "IMPORTS"
+            while i < len(words):
+                if words[i] != "FROM":
+                    imported_items.append(words[i])
+                else:
+                    imported_from = words[i+1]
+                    i += 1
+                    if imported_from.endswith(";"):
+                        parsing_imports = False
+                        imported_from = imported_from[:-1]
+                    for item in imported_items:
+                        imports[item] = imported_from
+                    imported_items = []
+                i += 1
+            continue
+        else:
             prev_line = line
             continue
-        elif line.startswith("OBJECT IDENTIFIER"):
-            # Let's take the previous line as well
-            line = prev_line + " " + line
-        match = re.search(r"([\w-]+)\s*OBJECT IDENTIFIER\s*::=\s*\{\s*([\w-]+)\s*([0-9]+)\s*\}", line)
-        name = match[1]
-        parent = match[2]
-        number = match[3]
-    elif line.startswith("IMPORTS"):
-        imported_items = []
-        parsing_imports = True
-        words = re.split(r"[, ]+", line)
-        if len(words) == 1:
-            continue
-        i = 1   # Skip the first word "IMPORTS"
-        while i < len(words):
-            if words[i] != "FROM":
-                imported_items.append(words[i])
-            else:
-                imported_from = words[i+1]
-                i += 1
-                if imported_from.endswith(";"):
-                    parsing_imports = False
-                    imported_from = imported_from[:-1]
-                for item in imported_items:
-                    imports[item] = imported_from
-                imported_items = []
-            i += 1
-        continue
-    else:
-        prev_line = line
-        continue
-    #print("{} = {{ {} {} }}".format(name, parent, number))
-    mib.add_item(name, parent, int(number))
-if mib:
-    all_mibs.append(mib)
+        #print("{} = {{ {} {} }}".format(name, parent, number))
+        mib.add_item(name, parent, int(number))
+    if mib:
+        all_mibs.append(mib)
+
+
+load_mibs()
 
 mibtree = Node("iso", ".1", "(root)")
+missing_items = set()
 for mib in all_mibs:
     for item in mib.items:
         if find_node(mibtree, item.name) or item.parent == "0":
